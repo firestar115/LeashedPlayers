5f91a6b85aed26bb9881326e1704c22b15a2397a
firestar115 <aprime2003@gmail.com>
2014-07-15 17:56:08 -0700

    Added Entity Leashers

--- LeashedPlayers/src/com/otabi/firestar/leashedplayers/LeashedPlayersListener.java
+++ LeashedPlayers/src/com/otabi/firestar/leashedplayers/LeashedPlayersListener.java
@@ -1,24 +1,58 @@
 package com.otabi.firestar.leashedplayers;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Effect;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.otabi.firestar.moolib.EntityUtils;
+
 public class LeashedPlayersListener implements Listener {
+	
+	public static HashMap<String, UUID> firstEntities = new HashMap<String, UUID>();
 	
 	public void onRightClick(PlayerInteractEvent evt){
 		if(evt.getAction() == Action.RIGHT_CLICK_AIR || evt.getAction() == Action.RIGHT_CLICK_BLOCK){
 			if(evt.getPlayer().getItemInHand().getType() == Material.LEASH){
-				for(Player p:Bukkit.getOnlinePlayers()){
-					if(evt.getPlayer().hasLineOfSight(p)){
-						p.setLeashHolder(evt.getPlayer());
-						p.getWorld().playEffect(p.getEyeLocation(), Effect.ENDER_SIGNAL, 31);
+				for(Entity e:EntityUtils.getAllEntities(evt.getPlayer(), 4)){
+					if(evt.getPlayer().hasLineOfSight(e)){
+						if(e instanceof Player){
+							Player p = (Player) e;
+							p.setLeashHolder(evt.getPlayer());
+						}
+						else if(e instanceof LivingEntity && evt.getPlayer().isSneaking()){
+							if(firstEntities.containsKey(evt.getPlayer().getDisplayName())){
+								for (Entity e1:((LivingEntity) e).getNearbyEntities(64, 64, 64)){
+									if(e1.getUniqueId() == firstEntities.get(evt.getPlayer().getName()) && e1 instanceof LivingEntity){
+										((LivingEntity) e1).setLeashHolder(e);
+									}
+								}
+								firstEntities.remove(evt.getPlayer().getDisplayName());
+							} else {
+								firstEntities.put(evt.getPlayer().getName(), e.getUniqueId());
+							}
+						}
 					}
 				}
+			}
+		}
+	}
+	
+	public void onEntityHitEntity(EntityDamageByEntityEvent evt){
+		Entity preleashed = evt.getEntity();
+		Entity preholder = evt.getDamager();
+		if(preleashed instanceof LivingEntity && preholder instanceof LivingEntity){
+			LivingEntity leashed = (LivingEntity) preleashed;
+			LivingEntity holder = (LivingEntity) preholder;
+			if(holder.getEquipment().getItemInHand().getType() == Material.LEASH){
+				leashed.setLeashHolder(holder);
 			}
 		}
 	}
